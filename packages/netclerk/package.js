fbbed22decec46eb8e927a3ef24d36ec2f3db235
Ryan Morrison~Westphal <ryan@trippingthebits.com>
2015-06-11 16:04:50 -0400

    netclerk - package file

---
+++ packages/netclerk/package.js
@@ -0,0 +1,27 @@
+Package.describe( {
+  name: 'netclerk',
+  version: '0.3.0',
+  summary: 'Ticker showing whether or not web content is being censored in various countries'
+} );
+
+Package.onuse( function( api ) {
+  api.versionsFrom( '1.1.0.2' );
+
+  api.use( [ 'widget', 'mongo' ] );
+
+  api.use( [ 'templating' ], 'client' );
+
+  //api.use( [ ], 'server' );
+
+  api.addFiles( [ 'netclerk.js' ] );
+
+  api.addFiles( [
+    'client/settings.html',
+    'client/settings.js',
+    'client/widget.html',
+    'client/widget.js',
+    'client/widget.css'
+  ], 'client' );
+    
+  api.export( 'NetClerk' );
+} );
