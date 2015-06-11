77b6b0d5bbb1588cbb9c4d93a4c19cfcc758cb8c
Ryan Morrison~Westphal <ryan@trippingthebits.com>
2015-06-11 13:01:26 -0400

    fewer widgets for faster startup/development

--- packages/imon-data/client/widget.js
+++
@@ -1,8 +0,0 @@
-Template.IMonWidget.onCreated(function() {
-  this.subscribe('imon_countries');
-});
-
-Template.IMonWidget.helpers({
-  indicatorPercent: function() { return this.indicator.value * 100; },
-  indicatorValue: function() { return Math.round(this.indicator.value * 100); },
-});
