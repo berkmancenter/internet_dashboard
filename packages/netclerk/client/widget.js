010681205325728ca5e0cfe9ee913b7b2d9cde5b
Ryan Morrison~Westphal <ryan@trippingthebits.com>
2015-06-17 15:36:57 -0400

    netclerk - nicer colors; shuffle data

--- packages/netclerk/client/widget.js
+++ packages/netclerk/client/widget.js
@@ -1,5 +1,25 @@
 Template.NetClerkWidget.helpers( {
-  statuses: function( ) { return RecentlyChanged.findOne().statuses.data; },
+  statuses: function( ) {
+    function shuffle(array) {
+      // Fisher-Yates (aka Knuth) Shuffle ( https://github.com/coolaj86/knuth-shuffle )
+      var currentIndex = array.length, temporaryValue, randomIndex ;
+
+      while (0 !== currentIndex) {
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex -= 1;
+
+        temporaryValue = array[currentIndex];
+        array[currentIndex] = array[randomIndex];
+        array[randomIndex] = temporaryValue;
+      }
+
+      return array;
+    }
+
+
+
+    return shuffle( RecentlyChanged.findOne().statuses.data );
+  },
   isUp: function( delta ) { return delta < 0 },
   pageDisplay: function( page ) {
     var l = document.createElement("a");
