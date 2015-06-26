24d73241f2b6daac189635739317b7c4572efe10
jdcc <jclark@cyber.law.harvard.edu>
2015-06-26 12:25:52 -0400

    flip chill axis

--- packages/lumen/client/widget.js
+++ packages/lumen/client/widget.js
@@ -14,25 +14,75 @@ Template.LumenWidget.onCreated(function() {
 Template.LumenWidget.onRendered(function() {
   var template = this;
   this.autorun(function() {
-    if (template.subscriptionsReady()) {
-      var urlCounts = [ {
-        label: 'URL Counts',
-        values: LumenCounts.find().map(function(bin) {
-          return { time: bin.start, y: bin.urlCount };
-        })
-      } ];
-
-      template.$('.url-counts').epoch({
-        type: 'time.bar',
-        data: urlCounts,
-        axes: ['left', 'bottom'],
-        ticks: { left: 2, time: Math.round(Settings.numBins / 3) },
-        tickFormats: { bottom: function(d) { return moment(d).fromNow(); } },
-        margins: { left: 30, top: 10, right: 20 },
-        windowSize: Settings.numBins,
-        historySize: Settings.numBins
-      });
+    if (!template.subscriptionsReady()) {
+      return;
     }
+
+    template.$('.url-counts').empty();
+
+    var urlCounts = LumenCounts.find().map(function(bin) {
+      return { x: bin.start, y: bin.urlCount };
+    });
+
+    var binStarts = _.map(urlCounts, function(d) { return d.x; });
+    var tickValues = function(binStarts, numTicks) {
+      var total = binStarts.length;
+      var step = Math.ceil(total / numTicks) || 0;
+      var ticks = [];
+      var i;
+      for (i = 0; i < total; i += step) {
+        ticks.push(binStarts[i]);
+      }
+      return ticks;
+    };
+
+    var margin = {top: 5, right: 20, bottom: 40, left: 30},
+        width = 300 - margin.left - margin.right,
+        height = 90 - margin.top - margin.bottom;
+
+    var x = d3.scale.ordinal()
+        .rangeRoundBands([0, width], 0.2);
+
+    var y = d3.scale.linear()
+        .range([height, 0]);
+
+    var xAxis = d3.svg.axis()
+        .scale(x)
+        .tickValues(tickValues(binStarts, 3))
+        .tickFormat(function(d) { return moment(d).fromNow(); })
+        .orient("bottom");
+
+    var yAxis = d3.svg.axis()
+        .scale(y)
+        .orient("left")
+        .ticks(3);
+
+    var svg = d3.select(template.find('.url-counts')).append("svg")
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom)
+      .append("g")
+        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+    x.domain(binStarts);
+    y.domain([0, d3.max(urlCounts, function(d) { return d.y; })]);
+
+    svg.append("g")
+        .attr("class", "x axis")
+        .attr("transform", "translate(0," + height + ")")
+        .call(xAxis);
+
+    svg.append("g")
+        .attr("class", "y axis")
+        .call(yAxis);
+
+    svg.selectAll(".bar")
+        .data(urlCounts)
+      .enter().append("rect")
+        .attr("class", "bar")
+        .attr("x", function(d) { return x(d.x); })
+        .attr("width", x.rangeBand())
+        .attr("y", function(d) { return y(d.y); })
+        .attr("height", function(d) { return height - y(d.y); });
   });
 });
 
