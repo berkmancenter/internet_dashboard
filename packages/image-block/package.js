ef0b7839c21533a0700f3c446933d9e97fb3919d
jdcc <jclark@cyber.law.harvard.edu>
2015-07-14 10:35:06 -0400

    needed remotely

--- packages/image-block/package.js
+++ packages/image-block/package.js
@@ -12,6 +12,7 @@ Package.onUse(function(api) {
   api.addFiles(['image-block.js']);
   api.addFiles([
     'client/widget.html',
+    'client/widget.css',
     'client/settings.html',
     'client/settings.js',
     'client/settings.css'
