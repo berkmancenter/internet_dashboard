4580edfafd2454825bcd9fd38872db609ae83a0a
jdcc <jclark@cyber.law.harvard.edu>
2015-07-21 18:07:57 -0400

    quote image urls

--- packages/user-content/client/widget.js
+++ packages/user-content/client/widget.js
@@ -2,7 +2,7 @@ Template.UserContentWidget.helpers({
   style: function() {
     var backgroundRule = 'background-color: ' + this.backgroundColor + ';';
     if (this.imageUrl) {
-      backgroundRule += 'background-image: url(' + this.imageUrl + '); ' +
+      backgroundRule += 'background-image: url("' + this.imageUrl + '"); ' +
         'background-size: cover;'; 
     }
     var textColorRule = 'color: ' + this.textColor + ';';
