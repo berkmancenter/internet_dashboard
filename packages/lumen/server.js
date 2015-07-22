d7a5620d833bef784b0077b99ef7c0329ed1f663
jdcc <jclark@cyber.law.harvard.edu>
2015-07-22 16:52:45 -0400

    change lumen to notice counts - #12176, #12398

--- packages/lumen/server.js
+++ packages/lumen/server.js
@@ -20,75 +20,68 @@ var getBins = function() {
   return bins;
 };
 
-var getNotices = function(page) {
-  page = page || Settings.startPage;
-  console.log('Lumen: Getting notices - page ' + page);
-
-  // Header auth
-  var options = {
-    headers: {
-      AUTHENTICATION_TOKEN: Settings.authToken,
-      Accept: 'application/json',
-      'Content-type': 'application/json'
-    },
-    params: {
-      sort_by: 'date_received desc',
-      per_page: Settings.perPage,
-      page: page
+var binRequest = function(bin) {
+  return { url: 'https://chillingeffects.org/notices/search',
+    options: {
+      headers: {
+        AUTHENTICATION_TOKEN: Settings.authToken,
+        Accept: 'application/json',
+        'Content-type': 'application/json'
+      },
+      params: {
+        per_page: 1,
+        date_received_facet: bin.end + '..' + bin.start
+      }
     }
   };
-
-  var url = 'https://chillingeffects.org/notices/search';
-  var result = HTTP.get(url, options);
-  return result.data.notices;
 };
 
-var selectValid = function(notices) {
-  var now = Date.now();
-  return _.filter(notices, function(notice) {
-    return Date.parse(notice.date_received) <= now;
-  });
-};
-
-var urlCount = function(notice) {
+var countNoticeUrls = function(notice) {
   return _.reduce(notice.works, function(memo, work) {
     return memo + (work.infringing_urls ? work.infringing_urls.length : 0);
   }, 0);
 };
 
+var countBinUrls = function(bin) {
+  var request = binRequest(bin);
+  request.options.params.per_page = Settings.perPage;
+  var totalPages = HTTP.get(request.url, request.options).data.meta.total_pages;
+
+  _(totalPages).times(function(i) {
+    var page = i + 1;
+    request.options.params.page = page;
+    console.log('Lumen: Fetching url counts (page ' + page + ' of ' + totalPages + 
+        ') from ' + new Date(bin.end).toUTCString() + ' to ' +
+        new Date(bin.start).toUTCString());
+    var result = HTTP.get(request.url, request.options);
+    _.each(result.data.notices, function(notice) {
+      bin.urlCount += countNoticeUrls(notice);
+    });
+  });
+  return bin.urlCount;
+}
+
 var updateLumenCounts = function() {
   var bins = getBins();
-  var currentBinI = 0;
-  var currentBin = bins[currentBinI];
-  var page = Settings.startPage;
-  var validNotices, noticeDate;
-
-  var updateBin = function(notice) {
-    noticeDate = Date.parse(notice.date_received);
-
-    if (noticeDate < currentBin.end) {
-      currentBinI += 1;
-    }
-
-    if (currentBinI < bins.length) {
-      currentBin = bins[currentBinI];
-      currentBin.urlCount += urlCount(notice);
-      currentBin.noticeCount += 1;
-    }
-  };
-
-  while (currentBinI < bins.length) {
-    validNotices = selectValid(getNotices(page));
-    _.each(validNotices, updateBin);
-    page += 1;
-  }
 
   LumenCounts.remove({});
+
   _.each(bins, function(bin) {
+    var request = binRequest(bin);
+    console.log('Lumen: Fetching notice count from ' +
+        new Date(bin.end).toUTCString() + ' to ' +
+        new Date(bin.start).toUTCString());
+    var result = HTTP.get(request.url, request.options);
+    bin.noticeCount = result.data.meta.total_entries;
+    if (Settings.countUrls) {
+      bin.urlCount = countBinUrls(bin);
+    }
     LumenCounts.insert(bin);
   });
 };
 
+// If we don't have any counts or our most recent is older than our update
+// interval...
 if (LumenCounts.find().count() === 0 ||
     LumenCounts.findOne({}, { sort: { start: -1 } }).start +
     Settings.updateEvery.asMilliseconds() < Date.now()) {
