5545c9d9fdafec830760f698600224f2b554a36a
jdcc <jclark@cyber.law.harvard.edu>
2015-08-13 11:48:56 -0400

    new dashboard button - #12573

--- client/templates/header.js
+++ client/templates/header.js
@@ -1,3 +1,9 @@
+Template.Header.helpers({
+  onDashboard: function() {
+    return Router.current() &&
+      Router.current().route.getName() === 'dashboards.show';
+  }
+});
 Template.Header.events({
   'click #sign-out': function() { Meteor.logout() }
 });
