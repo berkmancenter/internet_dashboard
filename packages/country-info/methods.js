78a72367961cfbd22f8d9dd767a8640cdf381eaf
jdcc <jclark@cyber.law.harvard.edu>
2015-08-27 11:34:25 -0400

    ensure uppercase

--- packages/country-info/methods.js
+++ packages/country-info/methods.js
@@ -2,7 +2,7 @@ countries = Npm.require('country-data').countries;
 
 Meteor.methods({
   countryByCode: function(code) {
-    var country = countries[code];
+    var country = countries[code.toUpperCase()];
     country.continent = _.find(continents, function(continent) {
       return _.contains(continent.countries, country.alpha2);
     });
