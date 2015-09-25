38f62bbfd47b6d9f46c5a4d3a12b54a04c308590
jdcc <jclark@cyber.law.harvard.edu>
2015-09-25 12:45:44 -0400

    not all widgets have positions

--- client/dashboards.js
+++ client/dashboards.js
@@ -25,7 +25,11 @@ Template.DashboardsShow.helpers({
   },
   width: function() {
     var maxCol = _.max(this.widgets().map(function(widget) {
-      return widget.position.col + widget.width - 1;
+      var col = 1;
+      if (widget.position && widget.position.col) {
+        col = widget.position.col;
+      }
+      return col + widget.width - 1;
     }));
     var totalWidth = maxCol * this.columnWidth + (maxCol + 2) * this.gutter;
     return totalWidth + 'px';
