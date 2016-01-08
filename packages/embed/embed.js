bc82b6c25dbe54f01e1b7439f02b4faaee04d19d
jdcc <jclark@cyber.law.harvard.edu>
2016-01-08 11:00:51 -0600

    make language style a bit more consistent across widgets

--- packages/embed/embed.js
+++ packages/embed/embed.js
@@ -11,7 +11,7 @@ EmbedWidget.prototype.constructor = EmbedWidget;
 Embed = {
   widget: {
     name: 'Embed',
-    description: 'Make a dashboard widget of something embeddable.',
+    description: 'Turns an embeddable object into a widget',
     dimensions: { width: 2, height: 2 },
     resize: { mode: 'reflow' },
     url: 'https://thenetmonitor.org',
