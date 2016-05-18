ba5ca86b6145d61dd3f23ac2805fe1683507b67c
Rebekah Heacock Jones <rebekah.heacock@gmail.com>
2016-05-18 16:16:10 -0400

    fix typos

--- client/home.js
+++ client/home.js
@@ -2,14 +2,6 @@ Template.Home.onCreated(function() {
   Meteor.subscribe('seeds');
 });
 
-Template.Home.Events(function() {
-	'click #template-background': function(ev, template) {
-    Meteor.call('copyDashboard', dashboard._id, function(error, newId) {
-      if (callback) { callback.apply(dashboard, [error, newId]); }
-    });
-  }
-});
-
 Template.Home.events({
   'click #template-background': function(ev, template) {
   	var templateboard = Dashboards.findOne({title: '*TEMPLATE: Country Background Information*'})
