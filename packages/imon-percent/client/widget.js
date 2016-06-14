e9ff114443b3f5fc1385b82de3b13697ed154187
Mariam Maarouf <mariam.mrf@hotmail.com>
2016-06-14 21:42:55 +0200

    Fix first render bug

--- packages/imon-percent/client/widget.js
+++ packages/imon-percent/client/widget.js
@@ -9,7 +9,7 @@ Template.IMonPercentWidget.onCreated(function() {
 });
 Template.IMonPercentWidget.onRendered(function(){
   var template = this;
-
+  var redrawn = false;
   template.autorun(function(){
     if (!template.subscriptionsReady()) { return; }
     // 1. Set indicator for the top right corner
@@ -23,6 +23,8 @@ Template.IMonPercentWidget.onRendered(function(){
     var widgetNode = template.firstNode.parentNode.parentNode;
     var $widgetBody = $(widgetNode).find('.widget-body');
     var node = template.find('.online-users');
+    var nodeRow = template.find('.nodeRow');
+    var container = template.find('.users-container');
     var title = template.find('.percent-title');
     var valuePlace = template.find('.indicator-value');
     var indicatorValue = IMonData.findOne({ sourceId: Template.currentData().indicatorId, countryCode: Template.currentData().country }).value;
@@ -33,9 +35,36 @@ Template.IMonPercentWidget.onRendered(function(){
 
     // 3. All the functions
     var setDims = function(){ // Set height for the container of the icons
-      var h =  $(widgetNode).height() - $('.title-bar', widgetNode).height() - $(node.parentNode.parentNode).position().top;
-      $(node).height(h);
+      var h =  $(widgetNode).innerHeight() - $('.title-bar', widgetNode).height() - $(nodeRow).position().top;
+      $(container).height(h);
       $(valuePlace.parentNode).height(h);
+    };
+
+    var readjust = function(){
+      var s = $(node).css('font-size').replace('px', '');
+      while(container.scrollHeight > container.clientHeight){
+        s*=0.9;
+        $(node).css('font-size', s + 'px');
+      }
+    };
+
+    var getFactor = function(base){
+      //    The factors are mainly from trial-and-error. Would rather just adjust/ reduce if there's overflow
+      //    instead of make it bigger until it can't be any bigger within the boundaries.
+      var factor = 0;
+      if(base<=10)
+        factor = 5;
+      else if (base<=15 && base>10)
+        factor = 3;
+      else if (base<=30 && base>15)
+        factor = 2.5;
+      else if (base<=50 && base>30)
+        factor = 2;
+      else if (base<=70 && base>50)
+        factor = 1.5;
+      else if (base<=90 && base>70)
+        factor = 1;
+      return factor;
     };
 
     var draw = function(){
@@ -50,10 +79,15 @@ Template.IMonPercentWidget.onRendered(function(){
       $('h2', title).text(countryName);
 
       // c. Draw
+      var sizeYNum = getFactor(base) + parseInt($(widgetNode).attr('data-sizey'));
+      var sizeY = sizeYNum + 'em';
+      $(node).css('font-size', sizeY);
+
       for(var i=0; i<base; i++){
         var colored = i < value ? 'colored' : 'plain';
         $(node).append('<i class="fa fa-' + icon + ' ' + colored + '"></i>');
       }
+
       $(valuePlace).css('font-size', '5em');
       if( format == 'percent'){
         var valueFull = indicatorValue.toFixed(0);
@@ -70,32 +104,11 @@ Template.IMonPercentWidget.onRendered(function(){
 
 
       // d. Set dimensions for icons
-      //    The factors are mainly from trial-and-error. Would rather just adjust/ reduce if there's overflow
-      //    instead of make it bigger until it can't be any bigger within the boundaries.
-      var factor = 0;
-      if(base<=10)
-        factor = 5;
-      else if (base<=15 && base>10)
-        factor = 3;
-      else if (base<=30 && base>15)
-        factor = 2.5;
-      else if (base<=50 && base>30)
-        factor = 2;
-      else if (base<=70 && base>50)
-        factor = 1.5;
-      else if (base<=90 && base>70)
-        factor = 1;
-      var sizeYNum = factor + parseInt($(widgetNode).attr('data-sizey'));
-      var sizeY = sizeYNum + 'em';
-      $(node).css('font-size', sizeY);
-
-      // e. Check if there is overflow
-      while(node.scrollHeight > node.clientHeight){
-        sizeYNum*=0.9;
-        $(node).css('font-size', sizeYNum + 'em');
-      }
+      readjust();
 
     };
+
+
 
     // 4. Event handlers
     $(widgetNode).on('gridster:resizestop', function() {
@@ -106,6 +119,15 @@ Template.IMonPercentWidget.onRendered(function(){
     // 5. Flow
     setDims();
     draw();
+
+    if(!redrawn){ // Yet to determine why a variation of this always works.
+      window.setTimeout(function(){
+        setDims();
+        draw();
+      }, 500);
+      redrawn = true;
+    }
+
   });
 });
 
