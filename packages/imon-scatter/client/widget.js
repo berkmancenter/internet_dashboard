4df059bc24b84e6c1a9a7a98713c33f49d6d85e2
Mariam <mariam.mrf@hotmail.com>
2016-07-04 07:38:23 +0200

    Move imon-scatter to new API

--- packages/imon-scatter/client/widget.js
+++ packages/imon-scatter/client/widget.js
@@ -5,9 +5,9 @@ Template.IMonScatterWidget.onCreated(function() {
       Template.currentData().x.indicator,
       Template.currentData().y.indicator
     ];
-    template.subscribe('imon_data', 'all', indicators,'id');
-    template.subscribe('imon_indicators');
-    template.subscribe('imon_countries');
+    template.subscribe('imon_data_v2', 'all', indicators, false);
+    template.subscribe('imon_indicators_v2');
+    template.subscribe('imon_countries_v2');
   });
 });
 
@@ -68,8 +68,8 @@ Template.IMonScatterWidget.onRendered(function() {
     if (!template.subscriptionsReady()) { return; }
     var xIndicator = Template.currentData().x.indicator;
     var yIndicator = Template.currentData().y.indicator;
-    var xTitle = IMonIndicators.findOne({ id: xIndicator }).shortName;
-    var yTitle = IMonIndicators.findOne({ id: yIndicator }).shortName; 
+    var xTitle = IMonIndicators.findOne({ adminName: xIndicator }).shortName;
+    var yTitle = IMonIndicators.findOne({ adminName: yIndicator }).shortName; 
     if (Template.currentData().x.log) {
       xTitle = 'Log ' + xTitle;
     }
@@ -79,11 +79,16 @@ Template.IMonScatterWidget.onRendered(function() {
 
     var data = [];
     IMonCountries.find().forEach(function(country) {
-      var x = IMonData.findOne({ countryCode: country.code, sourceId: xIndicator });
-      var y = IMonData.findOne({ countryCode: country.code, sourceId: yIndicator });
-      if (_.isUndefined(x) || _.isUndefined(y)) { return; }
+      var xValue, yValue;
+      IMonData.find({ countryCode: country.code, indAdminName: xIndicator }, { sort: { date: -1 }, limit: 1 }).forEach(function(d){
+        xValue = d.value;
+      });
+      var y;
+      IMonData.find({ countryCode: country.code, indAdminName: yIndicator }, { sort: { date: -1 }, limit: 1 }).forEach(function(d){
+        yValue = d.value;
+      });
+      if (_.isUndefined(xValue) || _.isUndefined(yValue)) { return; }
 
-      var xValue = x.value, yValue = y.value;
       if (Template.currentData().x.log && xValue > 0) {
         xValue = Math.log(xValue);
       }
@@ -99,6 +104,8 @@ Template.IMonScatterWidget.onRendered(function() {
         label: country.name
       });
     });
+
+    console.log(data);
 
     chart.margins(Settings.chart.margins);
     chart.responsive(false);
